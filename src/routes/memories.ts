468622f4ec3e01fd3605f633a46a8e6a09301617
Humberto <humberto.hechert2@hotmail.com>
2023-05-17 18:55:38 -0300

    criação inicial da rota GET /memories

---
+++ src/routes/memories.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        except: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/user/:id', async () => {})
+
+  app.post('/user', async () => {})
+
+  app.put('/user', async () => {})
+
+  app.delete('/user', async () => {})
+}
